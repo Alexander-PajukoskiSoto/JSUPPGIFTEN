f9ea34c29ab57c3f7de62baa3e19871f31e0bf80
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-09-30 20:28:43 +0200

    Arrays, obj, function with slesecor made

--- JS/bookStore.js
+++ JS/bookStore.js
@@ -1,2 +1,24 @@
 console.log("********BOOKSTORE START********")
 
+// The discounts
+const discount ={
+    1:1,
+    2:.95,
+    3:.90,
+    4:.80,
+    5:.75
+};
+
+// Array for the books
+const bookArray=[];
+
+// Array where discount combos are saved
+const disCombo = [];
+
+function bookCombos(a,b,c,d,e){
+    if(a+b+c+d+e >= 12){
+        console.log("hi")
+    }
+}
+
+console.log(bookCombos(2,0,0,0,0));
